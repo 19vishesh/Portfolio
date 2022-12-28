5c6ffdcd9d107ecdf5227df584d0f5f59e72f3ba
Vishesh-Kumar <31867166+19vishesh@users.noreply.github.com>
2022-12-28 15:48:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let icon = document.getElementById('icon')
 
+let loader = document.getElementById('preloader')
+
+window.addEventListener('load', function() {
+    loader.style.display = 'none'
+})
+
 icon.onclick = function() {
     document.body.classList.toggle('dark-theme')
     if(document.body.classList.contains('dark-theme')){
@@ -9,8 +15,13 @@ icon.onclick = function() {
     }
 }
 
-let loader = document.getElementById('preloader')
-
-window.addEventListener('load', function() {
-    loader.style.display = 'none'
+// For Auto-typing
+let typed = new Typed('.auto-type', 
+{
+    strings: ['नमस्ते', 'Hi', 'Hello', 'Holā', 'Nǐn hǎo'],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop:true
 })
+
+
